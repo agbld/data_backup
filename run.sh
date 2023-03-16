fe9865d4d9178e20e19d8494f59148d995a9146c
agiblida <agiblida@gmail.com>
2023-03-16 17:19:24 +0800

    08-1008-1008-1008-1008-1008-1008-1008-10

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
-python cp.py /home/jovyan/edu-smart-channel-research/jhju/samples/20220712/ ./20220712
-python split.py ./20220712
+python cp.py /home/jovyan/edu-smart-channel-research/jhju/samples/20220708/ ./20220708
+python split.py ./20220708
 
-python cp.py /home/jovyan/edu-smart-channel-research/jhju/samples/20220712/ ./20220713
-python split.py ./20220713
+python cp.py /home/jovyan/edu-smart-channel-research/jhju/samples/20220709/ ./20220709
+python split.py ./20220709
 
-python cp.py /home/jovyan/edu-smart-channel-research/jhju/samples/20220712/ ./20220714
-python split.py ./20220714
-
-python cp.py /home/jovyan/edu-smart-channel-research/jhju/samples/20220712/ ./20220715
-python split.py ./20220715
+python cp.py /home/jovyan/edu-smart-channel-research/jhju/samples/20220710/ ./20220710
+python split.py ./20220710
